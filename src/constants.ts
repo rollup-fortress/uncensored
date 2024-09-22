af62131bf446a2ba8d32e2b6a6650d48b7b1afe4
Anton Cheng <antonassocareer@gmail.com>
2024-09-22 20:46:30 +0800

    chore: add sepolia

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-import { optimism } from 'viem/chains';
+import { optimism, optimismSepolia } from 'viem/chains';
 import { Config, AdapterType } from './types';
 
 export const DEFAULT_CHAIN_CONFIGS: { [chainId: number]: Config } = {
@@ -6,5 +6,8 @@ export const DEFAULT_CHAIN_CONFIGS: { [chainId: number]: Config } = {
     type: AdapterType.OPStack,
     optimismPortalAddress: '0xbeb5fc579115071764c7423a4f12edde41f106ed',
   },
-  // Add other pre-configured chains here
+  [optimismSepolia.id]: {
+    type: AdapterType.OPStack,
+    optimismPortalAddress: '0x16Fc5058F25648194471939df75CF27A2fdC48BC',
+  },
 };
